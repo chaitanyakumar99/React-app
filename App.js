b6e409783b4604c0b8dd32cbee2468f4fda97f50
Your Name <gchkumar999@gmail.com>
2025-06-03 15:51:09 +0530

    txt

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 const heading = React.createElement("h1", {id:"c1"}, "Hello world inside React..")
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-
+console.log(heading)
 root.render(heading)
