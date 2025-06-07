b5021e265dd07f0fe983298b7783d003eaee4b80
Your Name <gchkumar999@gmail.com>
2025-06-07 20:06:45 +0530

    ok

--- App.js
+++ App.js
@@ -1,5 +1,16 @@
-const heading = React.createElement("h1", {id:"c1"}, "Hello world inside React..")
+/* <div id="parrent">
+    <div id="child">
+        <h1>GOod orning</h1>
+    </div>
+</div> */
+ 
+const parrent = React.createElement("div", { id: "parrent" },React.createElement("div", { id: "child" },React.createElement("h1",{},"HELLO GOOD MORNING ")) )
+
+// const heading = React.createElement("h1", { id: "c1" }, "Hello world inside React..")
+
+console.log(heading)  //object
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-console.log(heading)  //object
+
 root.render(heading)
