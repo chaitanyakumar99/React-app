d3d3da329e01a501e6dbe8380e3a093e988398bd
Your Name <gchkumar999@gmail.com>
2025-06-25 16:16:02 +0530

    ok

--- App.js
+++ App.js
@@ -2,15 +2,18 @@
     <div id="child">
         <h1>GOod orning</h1>
     </div>
-</div> */
+</div>
+
+ReactElement(object)=> HTML (Browser understands)
+*/
  
 const parrent = React.createElement("div", { id: "parrent" },React.createElement("div", { id: "child" },React.createElement("h1",{},"HELLO GOOD MORNING ")) )
 
 // const heading = React.createElement("h1", { id: "c1" }, "Hello world inside React..")
 
-console.log(heading)  //object
+// console.log(parrent)  //object
 
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+    const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(heading)
+root.render(parrent)     
--- one.js
+++ one.js
@@ -1,16 +1,6 @@
-// const heading= document.createElement("h1")
-// heading.innerHTML="Good morning react...!"
+const heading= document.createElement("h1")
+heading.innerHTML="Good morning react...!"
 
-// const r1=document.getElementById("root")
-// r1.appendChild(heading)
-
-
-
-
-
-
-
-
-
-
+const r1=document.getElementById("root")
+r1.appendChild(heading)
 
